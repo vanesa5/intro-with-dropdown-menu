fbb21661d37eb08e3a9039fbb9c1a18543c9f1a6
Vanesa G <87158362+vanesa5@users.noreply.github.com>
2024-07-18 11:18:30 -0400

    Work on dropdown menu

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const hamMenu = document.querySelector('.ham-menu');
 const offScreenMenu = document.querySelector('.off-screen-menu');
 
+// Drop Down Menu
+const companyTab = document.querySelector('.company');
+const companyDropDown = document.getElementById('company-menu')
+const featuresMenu = document.querySelector('.features')
+const featuresDropDown = document.getElementById('features-menu')
+
+
+
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
 })
+
+companyTab.addEventListener('click', () => {
+console.log("Company")
+    companyDropDown.removeAttribute('hidden');
+})
+
+featuresMenu.addEventListener('click', () => {
+    console.log("featues")
+        featuresDropDown.removeAttribute('hidden');
+    })
