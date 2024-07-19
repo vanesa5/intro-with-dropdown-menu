60f02d223dd62e9629bb97d980cfe700a74a5887
Vanesa G <87158362+vanesa5@users.noreply.github.com>
2024-07-19 12:19:11 -0400

    close and open dropdown menus

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Hamburger Menu
 const hamMenu = document.querySelector('.ham-menu');
 const offScreenMenu = document.querySelector('.off-screen-menu');
 
@@ -8,18 +9,29 @@ const featuresMenu = document.querySelector('.features')
 const featuresDropDown = document.getElementById('features-menu')
 
 
-
+// f-ham Menu
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
 })
 
+
+// f-dropdown menus
 companyTab.addEventListener('click', () => {
-console.log("Company")
-    companyDropDown.removeAttribute('hidden');
-})
+    console.log("Company")
+        if (companyDropDown.style.display === 'block'){
+            companyDropDown.style.display = 'none';
+        } else {
+            companyDropDown.style.display = 'block';
+
+        }
+    })
 
 featuresMenu.addEventListener('click', () => {
     console.log("featues")
-        featuresDropDown.removeAttribute('hidden');
-    })
+    if (featuresDropDown.style.display === 'block'){
+        featuresDropDown.style.display = 'none';
+    } else {
+        featuresDropDown.style.display = 'block';
+
+    }    })
